756b429c17aefcd24205be424bd89209b787a8b6
efmmoncada <emmanuel.moncada27@gmail.com>
2022-09-20 00:57:12 -0700

    add css and js to shift navbar add titles
    Signed-off-by: efmmoncada <emmanuel.moncada27@gmail.com>

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
+// enables deadlinks for top level menubar
 Array.from(document.querySelectorAll('.menuparent > a, #mobile-main-menu > ul > li > a')).forEach(function (link) {
     link.href = "";
     link.addEventListener('click', e => {
@@ -6,8 +7,22 @@ Array.from(document.querySelectorAll('.menuparent > a, #mobile-main-menu > ul > 
     });
 });
 
+
+function titleToInitials(title) {
+    let excludedWords = ['and', 'the', 'for', 'of'];
+    let titleWords = title.split(' ').filter(word => word !== '' && !excludedWords.includes(word.toLowerCase()) && /^[a-zA-Z]+$/.test(word));
+    return titleWords.map(word => word[0]).join('');
+}
+
+// TODO - figure out how to only do this for 30 chars or more, or maybe for all pages but make sure all abreivations are correct (HCE edge case)
 window.addEventListener('load', function () {
     let title = document.querySelector('h1 > a');
-    title.innerHTML += ` &raquo; ${document.querySelector('#page-title').textContent.toUpperCase()}`;
+    let pageTitle = document.querySelector('#page-title').textContent.toUpperCase().trim();
+    if (pageTitle.length > 25) {
+        pageTitle = titleToInitials(pageTitle);
+    }
+
+    title.parentElement.innerHTML += ` &raquo; ${pageTitle}`;
 });
+
 
